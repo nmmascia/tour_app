ef5e68232c995d76bfdc07f090ea9b86a0c5fbcd
Nicholas Mascia <nmmascia@Nicholass-MBP.fios-router.home>
2021-12-26 12:49:24 -0500

    Make Location a model with photo

--- server/src/entity/Location.ts
+++ server/src/entity/Location.ts
@@ -1,9 +1,39 @@
-import { Column } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  getRepository,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Photo } from "./Photo";
 
+@Entity()
 export class Location {
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Column({ nullable: false })
   name: string;
 
   @Column({ nullable: false })
   address: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  async avatar(): Promise<Photo> {
+    return await getRepository(Photo)
+      .createQueryBuilder("photo")
+      .where("photo.targetId = :targetId")
+      .where("photo.targetType = :targetType")
+      .setParameters({
+        targetId: this.id,
+        targetType: "Location",
+      })
+      .getOne();
+  }
 }
